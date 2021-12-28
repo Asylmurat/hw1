d1396e2fa56010535f0811dc9bbf2ffb2aad6360
Asylmurat <73055112+Asylmurat@users.noreply.github.com>
2021-12-28 22:29:53 +0600

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int c;
+    int arr[] = { 45, 1, 13, 26 };
+    for (int i = 0; i < 4; i++) {
+        if (arr[0] > arr[1]) {
+            c = arr[0];
+            arr[0] = arr[1];
+            arr[1] = c;
+        };
+        if (arr[1] > arr[2]) {
+            c = arr[1];
+            arr[1] = arr[2];
+            arr[2] = c;
+        };
+        if (arr[2] > arr[3]) {
+            c = arr[2];
+            arr[2] = arr[3];
+            arr[3] = c;
+        };
+        cout << arr[i] << " ";
+    }
+}
