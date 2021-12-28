406a5cb235d135c8b73f6403b32ed72228a7ae09
Asylmurat <73055112+Asylmurat@users.noreply.github.com>
2021-12-28 23:06:33 +0600

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k = 5;
+    int arr[] = { 1,3,5,6,9,15,17,56,89, 99 };
+    int left = 0;
+    int right = 9;
+    int middle;
+    while (left <= right) {
+        middle = (left + right) / 2;
+        if (arr[middle] > k) {
+            right = middle - 1;
+        }
+        else {
+            left = middle + 1;
+        }
+        if (arr[middle] == k) {
+            cout << middle;
+        }
+    }
+}
