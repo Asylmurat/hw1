5db49aa45fbd8c390fb6cf300888f655fe0fa0aa
Asylmurat <73055112+Asylmurat@users.noreply.github.com>
2021-12-29 21:05:28 +0600

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[50];
+    int i = 0;
+    int j = 0;
+    while (j < 50)
+    {
+        if (i % 2 != 0)
+        {
+            arr[j] = i;
+            j++;
+        }
+        i++;
+    }
+
+    for (int i = 0; i < 50; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+}
